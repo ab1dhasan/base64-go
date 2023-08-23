25e8cdc8336974b4748d847f026e1b0170ace177
Abid Hasan <abid@devsnest.net>
2023-08-23 10:59:43 +0600

    everything is ready

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+
+	"github.com/gabriel-vasile/mimetype"
+)
+
+func main() {
+	fileBytes, err := os.ReadFile(os.Args[len(os.Args)-1])
+	mimeType, mimeErr := mimetype.DetectFile(os.Args[len(os.Args)-1])
+
+	if err != nil || mimeErr != nil {
+		panic(err)
+	}
+
+	base64 := base64.StdEncoding.EncodeToString(fileBytes)
+
+	fmt.Print("data:" + mimeType.String() + ";base64," + base64)
+}
